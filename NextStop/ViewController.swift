3381a02579776a80dec734cee89d5c4e94b0048a
Yuchao CHEN <yuchao.chen@hotmail.com>
2019-08-15 18:49:58 +1000

    Initial Commit

---
+++ NextStop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NextStop
+//
+//  Created by Yuchao CHEN on 15/8/19.
+//  Copyright © 2019 Yuchao CHEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
