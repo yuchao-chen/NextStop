f55c3c81cc589537f96b1e6d8ebeebeb0f3eb5ac
Yuchao CHEN <yuchao.chen@hotmail.com>
2019-08-16 19:41:40 +1000

    Add routine list controller.

--- NextStop/ViewController.swift
+++ NextStop/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var startingPointTextfield: UIStackView!
+    @IBOutlet weak var destinationTextfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // MARK: Actions
+    @IBAction func goButton(_ sender: UIButton) {
+        
+    }
+    
 }
 
